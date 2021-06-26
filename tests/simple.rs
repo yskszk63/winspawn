0be9b9567b072f1e9faa574357c3ab4875e9565b
yskszk63 <yskszk63@gmail.com>
2021-06-26 19:41:03 +0900

    panic when failed

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,5 +1,4 @@
 use std::ffi::c_void;
-use std::io;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use winspawn::{spawn, swap_fd, FileDescriptor, Mode};
 
@@ -24,29 +23,27 @@ fn into_raw_handle<P>(_: P) -> HANDLE {
 }
 
 #[tokio::test]
-async fn test_simple() -> io::Result<()> {
-    let (rxtheir, mut txme) = tokio_anon_pipe::anon_pipe().await?;
-    let (mut rxme, txtheir) = tokio_anon_pipe::anon_pipe().await?;
+async fn test_simple() {
+    let (rxtheir, mut txme) = tokio_anon_pipe::anon_pipe().await.unwrap();
+    let (mut rxme, txtheir) = tokio_anon_pipe::anon_pipe().await.unwrap();
 
     let rxtheir = into_raw_handle(rxtheir);
     let txtheir = into_raw_handle(txtheir);
 
-    let rxtheir = FileDescriptor::from_raw_handle(rxtheir, Mode::ReadOnly)?;
-    let txtheir = FileDescriptor::from_raw_handle(txtheir, Mode::ReadWrite)?;
+    let rxtheir = FileDescriptor::from_raw_handle(rxtheir, Mode::ReadOnly).unwrap();
+    let txtheir = FileDescriptor::from_raw_handle(txtheir, Mode::ReadWrite).unwrap();
 
     let mut prog = swap_fd(rxtheir, 3, |_| {
         swap_fd(txtheir, 4, |_| spawn("python", ["./simple.rs"]))
-    })?;
+    }).unwrap();
 
-    txme.write_all(b"Hello").await?;
+    txme.write_all(b"Hello").await.unwrap();
     drop(txme);
 
     let mut buf = vec![];
-    rxme.read_to_end(&mut buf).await?;
+    rxme.read_to_end(&mut buf).await.unwrap();
     assert_eq!(b"Hello".as_ref(), &buf);
 
-    let exitcode = prog.wait()?;
+    let exitcode = prog.wait().unwrap();
     assert_eq!(0, exitcode);
-
-    Ok(())
 }
