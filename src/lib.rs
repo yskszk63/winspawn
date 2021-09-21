85e126e8ad27ef8f750f4110678dcf2268639767
yskszk63 <yskszk63@gmail.com>
2021-09-21 21:08:06 +0900

    change signature

--- src/lib.rs
+++ src/lib.rs
@@ -65,7 +65,7 @@ use sys::{_close, _dup, _dup2};
 use sys::{_wspawnvp, P_NOWAIT};
 use sys::{O_RDONLY, O_RDWR, O_WRONLY};
 
-use bindings::Windows::Win32::Foundation::{HANDLE, INVALID_HANDLE_VALUE};
+use bindings::Windows::Win32::Foundation::{HANDLE, INVALID_HANDLE_VALUE, BOOLEAN};
 use bindings::Windows::Win32::System::SystemServices::RTL_SRWLOCK;
 use bindings::Windows::Win32::System::Threading::{
     AcquireSRWLockExclusive, GetExitCodeProcess, InitializeSRWLock, RegisterWaitForSingleObject,
@@ -385,7 +385,7 @@ impl Future for Child {
     }
 }
 
-unsafe extern "system" fn callback(ptr: *mut c_void, _: u8) {
+unsafe extern "system" fn callback(ptr: *mut c_void, _: BOOLEAN) {
     let mut waker = Box::from_raw(ptr as *mut Option<Waker>);
     waker.take().unwrap().wake();
 }
